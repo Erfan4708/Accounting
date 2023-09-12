350ac9e673fb0160ab311c0318441020387c6c7b
Erfan <abbasy.4708@gmail.com>
2023-09-12 08:49:38 -0700

    Add project files.

---
+++ Accounting.DataLayer/Services/CustomerRepository.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accounting.DataLayer.Repositories;
+using System.Data.Entity;
+namespace Accounting.DataLayer.Services
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        Accounting_DBEntities2 db = new Accounting_DBEntities2();
+
+        public List<Customers> GetAllCustomers()
+        {
+            return db.Customers.ToList();
+        }
+
+        public Customers GetCustomerbyId(int customerid)
+        {
+            return db.Customers.Find(customerid);
+        }
+
+        public bool InsertCustomer(Customers customer)
+        {
+            try
+            {
+                db.Customers.Add(customer);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool UpdateCustomer(Customers customer)
+        {
+            try
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteCustomer(Customers customer)
+        {
+            try
+            {
+                db.Entry(customer).State = EntityState.Deleted;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteCustomer(int customerid)
+        {
+            try
+            {
+                var customer = GetCustomerbyId(customerid);
+                DeleteCustomer(customer);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+    }
+}
---
+++ DataLayer_Test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accounting.DataLayer;
+using Accounting.DataLayer.Repositories;
+using Accounting.DataLayer.Services;
+
+namespace DataLayer_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ICustomerRepository customer = new CustomerRepository();
+
+            Customers AddCustomer = new Customers()
+            {
+                FullName = "مریم تنابنده",
+                Mobile = "09128456985",
+                Email = "maryam@maryam.com",
+                Address = "تهران",
+                CustomerImage = "NoPhoto"
+            };
+            customer.InsertCustomer(AddCustomer);
+            customer.Save();
+
+            var list = customer.GetAllCustomers();
+            
+        }
+    }
+}
